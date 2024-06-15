984c34aeb26c2c78a39f7066d1099727eeb74a17
Martín Stanicio <martuytimo@gmail.com>
2024-06-15 00:20:08 -0300

    vault backup: 2024-06-15 00:20:08

--- Algoritmos y Estructuras de Datos/python/20240614-hipotenusa.py
+++ Algoritmos y Estructuras de Datos/python/20240614-hipotenusa.py
@@ -2,7 +2,8 @@
 # Autor: Martín Stanicio
 # Fecha: 14/06/2024
 
+
 def pitagoras(opuesto: float, adyacente: float):
-    hipotenusa = (opuesto ** 2 + adyacente ** 2) ** 0.5
-    
+    hipotenusa = (opuesto**2 + adyacente**2) ** 0.5
+
     return hipotenusa
--- Algoritmos y Estructuras de Datos/python/20240614-rectangulo.py
+++ Algoritmos y Estructuras de Datos/python/20240614-rectangulo.py
@@ -2,11 +2,10 @@
 # Autor: Martín Stanicio
 # Fecha: 14/06/2024
 
+
 def rectangulo(largo: int, altura: int):
     for i in range(altura):
         if i == 0 or i == altura - 1:
-            print("*"*largo)
+            print("*" * largo)
         else:
-            print("*" + " "*(largo - 2) + "*")
-            
-rectangulo(4, 5)
+            print("*" + " " * (largo - 2) + "*")
