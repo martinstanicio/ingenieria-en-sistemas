1662b35d6bcefd9309ec242f2dc9dfdf963b3dcf
Martín Stanicio <martuytimo@gmail.com>
2024-09-09 16:31:25 -0300

    vault backup: 2024-09-09 16:31:24

--- Algoritmos y Estructuras de Datos/python/20240909-alumnos.py
+++ Algoritmos y Estructuras de Datos/python/20240909-alumnos.py
@@ -1,3 +1,28 @@
 # AyED
 # Autor: Martín Stanicio
 # Fecha: 09/09/2024
+
+alumnos = int(input("Cantidad de alumnos: "))
+apellidos = [""] * alumnos
+legajos = [0] * alumnos
+nacimientos = [""] * alumnos
+notas = [0.0] * alumnos
+
+max_nota = -1.0
+
+for i in range(0, alumnos):
+    print(f"Alumno {i + 1}")
+    apellidos[i] = input("Apellido: ")
+    legajos[i] = int(input("Legajo: "))
+    nacimientos[i] = input("Fecha de nacimiento (dd/mm/aaaa): ")
+    notas[i] = float(input("Nota: "))
+    print()
+
+    if notas[i] > max_nota:
+        max_nota = notas[i]
+
+print(f"Nota máxima: {max_nota}")
+
+for i in range(0, alumnos):
+    if notas[i] == max_nota:
+        print(f"{apellidos[i]}, {legajos[i]}, {nacimientos[i]}")
