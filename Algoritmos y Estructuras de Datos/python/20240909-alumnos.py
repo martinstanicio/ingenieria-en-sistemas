836c22b00804f835d9828fa57e90262c46e37f41
Martín Stanicio <martuytimo@gmail.com>
2024-09-09 17:01:24 -0300

    vault backup: 2024-09-09 17:01:24

--- Algoritmos y Estructuras de Datos/python/20240909-alumnos.py
+++ Algoritmos y Estructuras de Datos/python/20240909-alumnos.py
@@ -25,4 +25,50 @@ print(f"Nota máxima: {max_nota}")
 
 for i in range(0, alumnos):
     if notas[i] == max_nota:
-        print(f"{apellidos[i]}, {legajos[i]}, {nacimientos[i]}")
+        mes = nacimientos[i][3:5]
+        print(f"Apellido {apellidos[i]}, Legajo {legajos[i]}, Mes {mes}")
+
+print()
+
+
+def insertion_sort(vector):
+    n = len(vector)
+
+    for i in range(0, n - 1):
+        for j in range(i + 1, n):
+            if vector[i] > vector[j]:
+                aux = vector[i]
+                vector[i] = vector[j]
+                vector[j] = aux
+
+    return vector
+
+
+def bubble_sort(vector):
+    n = len(vector)
+
+    for _ in range(0, n - 1):
+        for i in range(0, n - 1):
+            if vector[i] > vector[i + 1]:
+                aux = vector[i]
+                vector[i] = vector[i + 1]
+                vector[i + 1] = aux
+
+    return vector
+
+
+notas_ordenadas = insertion_sort(notas)
+control = notas_ordenadas[0]
+z = 0
+
+for i in range(0, len(notas_ordenadas)):
+    nota = notas_ordenadas[i]
+
+    if control == nota:
+        z += 1
+    else:
+        print(f"La nota {control} se repite {z} veces")
+        control = nota
+        z = 1
+
+print(f"La nota {control} se repite {z} veces")
