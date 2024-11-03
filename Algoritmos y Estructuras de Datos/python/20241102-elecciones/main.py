5da0364aaa0461463fb13be8709f985eb3fc2ca3
Martín Stanicio <martuytimo@gmail.com>
2024-11-03 11:53:08 -0300

    vault backup: 2024-11-03 11:53:08

--- Algoritmos y Estructuras de Datos/python/20241102-elecciones/main.py
+++ Algoritmos y Estructuras de Datos/python/20241102-elecciones/main.py
@@ -6,9 +6,9 @@ archivo = open("elecciones.txt", "r")
 registro = archivo.readline().strip()
 vector = registro.split(";")
 
-max_milei = 0
+max_milei = -1
 ciudad_milei = ""
-max_massa = 0
+max_massa = -1
 ciudad_massa = ""
 
 
@@ -32,17 +32,18 @@ while registro != "":
         print(f"Votos de Milei: {z_milei}")
         print(f"Votos de Massa: {z_massa}")
         print("")
+
+        if z_milei > max_milei:
+            max_milei = z_milei
+            ciudad_milei = ciudad
+
+        if z_massa > max_massa:
+            max_massa = z_massa
+            ciudad_massa = ciudad
+
         control = ciudad
         z_milei = v_milei
         z_massa = v_massa
-
-    if v_milei > max_milei:
-        max_milei = v_milei
-        ciudad_milei = ciudad
-
-    if v_massa > max_massa:
-        max_massa = v_massa
-        ciudad_massa = ciudad
 
     registro = archivo.readline().strip()
 
@@ -52,7 +53,6 @@ print(f"Ciudad: {control}")
 print(f"Votos de Milei: {z_milei}")
 print(f"Votos de Massa: {z_massa}")
 
-print("")
-
-print(f"Ciudad con mas votos de Milei: {ciudad_milei}")
-print(f"Ciudad con mas votos de Massa: {ciudad_massa}")
+# print("")
+# print(f"Ciudad con mas votos de Milei: {ciudad_milei}")
+# print(f"Ciudad con mas votos de Massa: {ciudad_massa}")
