cf0638b4e96c26f55fbd8b787101efcd7f97f9b5
Martín Stanicio <martuytimo@gmail.com>
2024-11-02 21:01:09 -0300

    vault backup: 2024-11-02 21:01:09

--- Algoritmos y Estructuras de Datos/python/20241102-elecciones/main.py
+++ Algoritmos y Estructuras de Datos/python/20241102-elecciones/main.py
@@ -6,8 +6,34 @@ archivo = open("elecciones.txt", "r")
 registro = archivo.readline().strip()
 vector = registro.split(";")
 
-z = 0
+z_milei = 0
+z_massa = 0
 control = vector[0]
 
 while registro != "":
     vector = registro.split(";")
+
+    ciudad = vector[0]
+    n_mesa = int(vector[1])
+    v_milei = int(vector[2])
+    v_massa = int(vector[3])
+
+    if ciudad == control:
+        z_milei += v_milei
+        z_massa += v_massa
+    else:
+        print(f"Ciudad: {control}")
+        print(f"Votos de Milei: {z_milei}")
+        print(f"Votos de Massa: {z_massa}")
+        print("")
+        control = ciudad
+        z_milei = 0
+        z_massa = 0
+
+    registro = archivo.readline().strip()
+
+archivo.close()
+
+print(f"Ciudad: {control}")
+print(f"Votos de Milei: {z_milei}")
+print(f"Votos de Massa: {z_massa}")
