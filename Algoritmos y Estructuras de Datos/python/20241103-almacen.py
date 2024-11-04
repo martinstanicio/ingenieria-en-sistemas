96491b8cf8fd2c74dec29f155e469b14c822fe89
Martín Stanicio <martuytimo@gmail.com>
2024-11-03 22:28:10 -0300

    vault backup: 2024-11-03 22:28:09

--- Algoritmos y Estructuras de Datos/python/20241103-almacen.py
+++ Algoritmos y Estructuras de Datos/python/20241103-almacen.py
@@ -3,6 +3,9 @@
 # Fecha: 03/11/2024
 
 
+from random import randint
+
+
 def bubble_sort(vector):
     n = len(vector)
 
@@ -14,3 +17,48 @@ def bubble_sort(vector):
                 vector[i + 1] = aux
 
     return vector
+
+
+def binary_search(vector, target: int) -> int:
+    n = len(vector)
+    ans = -1
+
+    high = n - 1
+    low = 0
+    found = False
+
+    while not found and target >= vector[low] and target <= vector[high]:
+        mid = (high + low) // 2
+
+        if vector[mid] == target:
+            ans = mid
+            found = True
+        elif target > vector[mid]:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return ans
+
+
+elementos = 10
+vector = [0] * elementos
+
+for i in range(0, elementos):
+    vector[i] = randint(100, 999)
+
+vector = bubble_sort(vector)
+
+for i in range(0, elementos):
+    if i > 0:
+        print(", ", end="")
+    print(vector[i], end="")
+print()
+
+target = int(input("Ingrese el número de ID a buscar: "))
+ans = binary_search(vector, target)
+
+if ans == -1:
+    print(f"No se ha encontrado ningún artículo con ID {target}.")
+else:
+    print(f"El artículo está en la posición {ans}.")
